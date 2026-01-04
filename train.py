e88adb6520d2a85db61a79ae3797f801d0d22821
mekdmehta1997-dev <mekdmehta1997@gmail.com>
2026-01-04 15:43:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,20 +5,13 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
-# Load dataset
 df = pd.read_csv("AirPassengers.csv")
-
-# Rename columns for clarity
 df.columns = ["Month", "Passengers"]
 
-# Create time index
 df["t"] = np.arange(len(df))
-
 X = df[["t"]]
 y = df["Passengers"]
 
-# MLflow tracking
-mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment("AirPassengers-Regression")
 
 with mlflow.start_run():
